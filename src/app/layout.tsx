9e2695baa066c3dd5c8b79a43572cc4f9f35698f
Ramon Ochoa Guerra <rochoaw10@gmail.com>
2026-02-12 12:33:43 -0500

    feat: Change script without recaptcha

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
           src='https://statics.orishiacore.com/connet/webchat/production/widget.js'
           strategy='afterInteractive'
           // @ts-expect-error: 'config' is a custom property required by Helpia widget
-          config='eyJjb25maWdJZCI6InI0cGEtLWVhNHBxeHVhdSIsIndzVG9rZW4iOiJxTU53QVEwVlFYeXlpekJxeXZJcnpodWdwaHZSUUg0Qko0d1Q2YXJjQVpMdE0iLCJiYWNrU2VydmVyIjoiaHR0cHM6Ly9iYWNrZW5kLmNvbm5ldC5vcmlzaGlhY29yZS5jb20iLCJ3c1NlcnZlciI6IndzczovL3dzLmJhY2tlbmQuY29ubmV0Lm9yaXNoaWFjb3JlLmNvbS9jaGFubmVscyIsImVuYWJsZVJlY2FwdGNoYSI6dHJ1ZX0='
+          config='eyJjb25maWdJZCI6InI0cGEtLWVhNHBxeHVhdSIsIndzVG9rZW4iOiJxTU53QVEwVlFYeXlpekJxeXZJcnpodWdwaHZSUUg0Qko0d1Q2YXJjQVpMdE0iLCJiYWNrU2VydmVyIjoiaHR0cHM6Ly9iYWNrZW5kLmNvbm5ldC5vcmlzaGlhY29yZS5jb20iLCJ3c1NlcnZlciI6IndzczovL3dzLmJhY2tlbmQuY29ubmV0Lm9yaXNoaWFjb3JlLmNvbS9jaGFubmVscyIsImVuYWJsZVJlY2FwdGNoYSI6ZmFsc2V9'
         />
       </body>
     </html>
